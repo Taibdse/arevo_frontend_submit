563a3e829708fff88f4dadb9e17f8936ffb40d4b
Taibdse <buiductai232@gmail.com>
2020-06-14 15:20:38 +0700

    add sort data

--- src/index.js
+++ src/index.js
@@ -5,49 +5,55 @@ import "./styles.css";
 
 // the height of each segment
 const segmentHeight = 12; //px
-//the space between levels
-const spaceBetweenLevels = 4; //px
+//the space between rows
+const spaceBetweenrows = 4; //px
 
-// level is the row that segments lie in and start from 0
+// row is the one that segments lie in and start from 0
 // for example: in the timeSegmentsNums.png, 
-// the segment 0-60 is in level 0, segment 240-320 is in level 1; segment 280-410 is in level 2
+// the segment 0-60 is in row 0, segment 240-320 is in row 1; segment 280-410 is in row 2
 
 // assume all data is valid and sorted by start time
 const TimelineSegments = ({ data, totalTrackLength }) => {
   
-  // [200, 400, 500] 200 is the max value in level 0
-  const maxLevelValues = [];
+  //sort the segments that have the same start value and their end values are not sorted in ascending; 
+  data.sort((a, b) => {
+    if(a.start === b.start && a.end < b.end) return -1;
+    return 0;
+  });
+
+  // [200, 400, 500]: 200 is the max value in row 0
+  const maxRowValues = [];
 
   const renderedData = data.map((item, index) => {
     item.width = (item.end - item.start) / totalTrackLength * 100;
     item.left = item.start / totalTrackLength * 100;
 
     if(index === 0) {
-      item.level = 0;
-      maxLevelValues.push(item.end);
+      item.row = 0;
+      maxRowValues.push(item.end);
     } else {
-      //find the level for current segment
-      for(let i = 0; i < maxLevelValues.length; i++){
-        const maxValueForLevelI = maxLevelValues[i];
-        if(item.start > maxValueForLevelI) {
-          item.level = i;
-          if(item.end > maxValueForLevelI) maxLevelValues[i] = item.end;
+      //find the row for current segment
+      for(let i = 0; i < maxRowValues.length; i++){
+        const maxValueForRowI = maxRowValues[i];
+        if(item.start > maxValueForRowI) {
+          item.row = i;
+          if(item.end > maxValueForRowI) maxRowValues[i] = item.end;
           break;
         } 
       }
      
-      //if the current segment has no level to lie in, start the next level
-      if(item.level === undefined || item.level === null){
-        item.level = maxLevelValues.length;
-        maxLevelValues.push(item.end);
+      //if the current segment has no row to lie in, start the next row
+      if(item.row === undefined || item.row === null){
+        item.row = maxRowValues.length;
+        maxRowValues.push(item.end);
       }
     }
 
-    item.top = item.level * (segmentHeight + spaceBetweenLevels);
+    item.top = item.row * (segmentHeight + spaceBetweenrows);
     return { ...item };
   })
 
-  const containerHeight = maxLevelValues.length * (segmentHeight + spaceBetweenLevels) - spaceBetweenLevels;
+  const containerHeight = maxRowValues.length * (segmentHeight + spaceBetweenrows) - spaceBetweenrows;
 
   return <div className="container" style={{ height: containerHeight + 'px' }}>
     {renderedData.map(item => (
